11c39b6e869ba8ce933ec9dd342ab9b8bb59091f
hongjungwoo12 <hongjungwoo0405@gmail.com>
2023-04-05 18:26:57 +0900

    4/5 문자열 슬라이싱까지 완료

---
+++ basic/01_string.py
@@ -0,0 +1,43 @@
+# eng = "abcdefg"
+# kor = "가나다라마바사"
+
+# print(eng[3])
+# print(f"{kor[-1:-3:-1]}! 이 자식아")
+
+# print(eng[::-1])
+
+
+# name = ""
+# age = 0
+# name = input("이름은? : ")
+# age = input("나이는? : ")
+
+
+
+# if int (age) <= 15 :
+
+#     print(f"{name[1:3]} 너 까불지마")
+# elif int (age) >= 16 :
+#     print(f"{name[0]}사장님")
+
+import random
+
+
+name = "라라라라라라"
+call = ["바보", "천재", "미남", "추남", "또라이"]
+print(f"역시! {name[3:]} {call[random.randint(0, 4)]}!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
